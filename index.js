2075e548ebdb4193a9b5671d105c211273d061e7
oscarotero <oom@oscarotero.com>
2015-09-10 02:27:52 +0200

    space indentation

--- index.js
+++ index.js
@@ -2,63 +2,63 @@
 
 module.exports = function (tasks) {
 
-	tasks.addTask({
-		filter: {
-			type: 'Function',
-			name: 'calc'
-		},
-		fn: function (fn) {
-			resolve(fn);
-		}
-	});
+    tasks.addTask({
+        filter: {
+            type: 'Function',
+            name: 'calc'
+        },
+        fn: function (fn) {
+            resolve(fn);
+        }
+    });
 
-	tasks.addTask({
-		filter: 'Expression',
-		fn: function (expression) {
-			resolve(expression);
-		}
-	});
+    tasks.addTask({
+        filter: 'Expression',
+        fn: function (expression) {
+            resolve(expression);
+        }
+    });
 
-	function resolve (element) {
-		var mainUnit, units = element.getAll('Unit');
+    function resolve (element) {
+        var mainUnit, units = element.getAll('Unit');
 
-		//Check the unit used in this function
-		units.forEach(function (unit) {
-			if (mainUnit === false) {
-				return;
-			}
+        //Check the unit used in this function
+        units.forEach(function (unit) {
+            if (mainUnit === false) {
+                return;
+            }
 
-			if (mainUnit === undefined) {
-				mainUnit = unit.name;
-			} else if (mainUnit !== unit.name) {
-				mainUnit = false;
-			}
-		});
+            if (mainUnit === undefined) {
+                mainUnit = unit.name;
+            } else if (mainUnit !== unit.name) {
+                mainUnit = false;
+            }
+        });
 
-		//There are more than one unit
-		if (mainUnit === false) {
-			return;
-		}
+        //There are more than one unit
+        if (mainUnit === false) {
+            return;
+        }
 
-		//Replace all units by its numbers
-		units.forEach(function (unit) {
-			var number = unit.get('Number');
+        //Replace all units by its numbers
+        units.forEach(function (unit) {
+            var number = unit.get('Number');
 
-			if (unit.name === '%') {
-				number.name *= 0.01;
-			}
+            if (unit.name === '%') {
+                number.name *= 0.01;
+            }
 
-			unit.replaceWith(unit.get('Number'));
-		});
+            unit.replaceWith(unit.get('Number'));
+        });
 
-		var number = eval(element.join(' '));
+        var number = eval(element.join(' '));
 
-		if (mainUnit === undefined) {
-			element.replaceWithCode(number.toFixed(2), 'Number');
-		} else if (mainUnit === '%') {
-			element.replaceWithCode((number *= 100).toFixed(2) + '%', 'Unit');
-		} else {
-			element.replaceWithCode(number.toFixed(2) + mainUnit, 'Unit');
-		}
-	}
+        if (mainUnit === undefined) {
+            element.replaceWithCode(number.toFixed(2), 'Number');
+        } else if (mainUnit === '%') {
+            element.replaceWithCode((number *= 100).toFixed(2) + '%', 'Unit');
+        } else {
+            element.replaceWithCode(number.toFixed(2) + mainUnit, 'Unit');
+        }
+    }
 };
